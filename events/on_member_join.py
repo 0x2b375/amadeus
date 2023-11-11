357c3ebd47207998c23267934068be16cc667fef
0xb39 <giiguu.giigu@gmail.com>
2023-11-12 02:04:05 +0800

    modified welcome message with pil

--- events/on_member_join.py
+++ events/on_member_join.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-
+from easy_pil import Editor, load_image_async, Font
+from discord import File
 
 class OnMemberJoinCog(commands.Cog):
     def __init__(self, bot):
@@ -9,17 +10,23 @@ class OnMemberJoinCog(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel_id = 786220835974938668
-        color_bot = discord.Colour.from_rgb(141, 9, 9)
-        welcome_message = f"Эрхэм {member.mention} танаа манай server-t нэгдсэнд баяр хүргэн мэндчилж байна XD"
+        member_count = len(member.guild.members)
         channel = member.guild.get_channel(channel_id)
         if channel:
-            embed = discord.Embed(title="",
-                                  description=welcome_message,
-                                  color=color_bot)
-            embed.set_image(
-                url='https://media.tenor.com/rK3k9EgLkhEAAAAC/steins-gate.gif')
-            message = await channel.send(embed=embed)
-            await message.add_reaction("👋")
+            background = Editor("images/hello.jpg")
+            profile_image = await load_image_async(str(member.avatar.url))
+            profile = Editor(profile_image).resize((150,150)).circle_image()
+            poppins = Font.poppins(size=50, variant="bold")
+            
+            poppins_small = Font.poppins(size=20, variant="light")
+            background.paste(profile, (325, 90))
+            background.ellipse((325, 90), 150, 150, outline="white", stroke_width=5)
+            
+            background.text((400, 260), f"LAB MEMBER {member_count}" , color="white", font=poppins, align="center")
+            background.text((400, 325), f"{member.name}#{member.discriminator}", color="white", font=poppins_small, align="center")
+            file = File(fp=background.image_bytes, filename="images/hello.jpg")
+            await channel.send(f"**YOUKOSOU {member.mention}!**")
+            await channel.send(file=file)
 
 
 async def setup(bot):
